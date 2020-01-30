6586410cc196f18e3362f83fbca7fb3b4b328a1f
husihui <husihui-jk@360jinrong.net>
2020-01-30 22:08:05 +0800

    添加Eslint

--- components/public/header/geo.vue
+++ components/public/header/geo.vue
@@ -1,6 +1,18 @@
 <template>
   <div class="m-geo">
     <i class="el-icon-location"></i> 北京市
-    <nuxt-link class="changeCity" to="changeCity"></nuxt-link>
+    <nuxt-link class="changeCity" to="changeCity">切换城市</nuxt-link>
+    [金华 永康 义乌]
   </div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  
+})
+</script>
+
+<style lang="sass" scoped>
+
+</style>
---
+++ components/public/header/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="m-header">
+    <el-row>
+      <el-col>
+        <top-bar />
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col>
+        <search-bar />
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import topBar from './topbar.vue'
+import searchBar from './searchbar.vue'
+export default Vue.extend({
+  components: { topBar, searchBar }
+})
+</script>
+
+<style lang="scss">
+  @import "@/assets/css/public/header/index.scss";
+</style>
