7744ad925bb13312e344c1abfae09d60f923a061
husihui <husihui-jk@360jinrong.net>
2020-01-31 03:29:10 +0800

    分类侧边栏

--- components/index/menu.vue
+++ components/index/menu.vue
@@ -1,22 +1,44 @@
 <template>
   <div class="m-menu">
-    <el class="nav">
+    <dl
+      class="nav"
+      @mouseleave="mouseleave"
+    >
       <dt>全部分类</dt>
       <dd
-        v-for="(item, index) in menu"
-        :key="index"
+        v-for="(item,idx) in menu"
+        :key="idx"
+        @mouseenter="enter"
       >
-        <i :class="item.type" /> {{ item.name }}<span class="arrow" />
+        <i :class="item.type" />{{ item.name }}<span class="arrow" />
       </dd>
-    </el>
+    </dl>
+    <div
+      v-if="kind"
+      class="detail"
+      @mouseenter="sover"
+      @mouseleave="sout"
+    >
+      <template
+        v-for="(item,idx) in curdetail.child"
+      >
+        <h4 :key="idx">
+          {{ item.title }}
+        </h4>
+        <span
+          v-for="v in item.child"
+          :key="v"
+        >{{ v }}</span>
+      </template>
+    </div>
   </div>
 </template>
 
-<script lang="ts">
-import Vue from 'vue'
-export default Vue.extend({
+<script>
+export default {
   data () {
     return {
+      kind: '',
       menu: [{
         type: 'food',
         name: '美食',
@@ -40,6 +62,31 @@ export default Vue.extend({
         }]
       }]
     }
+  },
+  computed: {
+    curdetail: function () {
+      return this.menu.filter(item => item.type === this.kind)[0]
+    }
+  },
+  methods: {
+    mouseleave: function () {
+      const self = this
+      self._timer = setTimeout(function () {
+        self.kind = ''
+      }, 150)
+    },
+    enter: function (e) {
+      this.kind = e.target.querySelector('i').className
+    },
+    sover: function () {
+      clearTimeout(this._timer)
+    },
+    sout: function () {
+      this.kind = ''
+    }
   }
-})
+}
 </script>
+
+<style lang="css">
+</style>
